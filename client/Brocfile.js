398da8cef2cf2794108da6555a86ace88bbce6da
Michael Choate <choatemd@miamioh.edu>
2015-02-06 03:01:59 -0500

    Stylistic changes & placeholder edit buttons

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -20,5 +20,8 @@ var app = new EmberApp();
 // Reference dependencies here
 app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+  destDir: 'fonts'
+});
 
 module.exports = app.toTree();
