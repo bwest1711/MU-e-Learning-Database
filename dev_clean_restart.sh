3f5d8157f1442925fc0392383c330bd8323628cb
Michael Choate <choatemd@miamioh.edu>
2015-02-06 00:32:02 -0500

    Changed dev restart script

---
+++ dev_clean_restart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# NOTE: For development convenience only. This should be removed from the
+# production server IMMEDIATELY to avoid accidents. 
+echo 'Removing old database.'
+rm app.db
+echo 'Setting up new database.'
+env/bin/python init_db.py
+echo 'Inserting test data.'
+env/bin/python insert_test_data.py
+echo 'Starting server.'
+env/bin/python run_dev.py
