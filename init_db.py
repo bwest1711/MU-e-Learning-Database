f9d48857107c382a3e104759237cbee8f7e350b1
Michael Choate <choatemd@miamioh.edu>
2015-02-09 02:50:57 -0500

    Cleaned up old Todo app

--- init_db.py
+++ init_db.py
@@ -1,6 +1,5 @@
 from main import create_app
 from database import db
-from api.models.todo import Todo
 from api.models.course import Course
 from api.models.course_version import CourseVersion
 from api.models.course_section import CourseSection
--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from database import db
 from flask import Flask
 
 from client.views import client_app
-from todo.views import root_app
 from api.routes import api_app
-
 
 def create_app():
     '''Initialize Flask and SQLAlchemy contexts and register blueprints'''
@@ -15,6 +13,5 @@ def create_app():
     db.init_app(app)
 
     app.register_blueprint(client_app)
-    app.register_blueprint(root_app)
     app.register_blueprint(api_app)
     return app
