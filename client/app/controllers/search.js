96a224ac3b92973d106169b19ef5e75cbc1bede2
Michael Choate <choatemd@miamioh.edu>
2015-02-14 15:59:57 -0500

    Restructure search controller for API search results

--- client/app/controllers/search.js
+++ client/app/controllers/search.js
@@ -1,19 +1,31 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ObjectController.extend({
+  queryParams: ['title'],
+  title: "",
+
+  filteredCourseVersions: function() {
+    var self = this;
+
+    var filters = [
+      { "name": "label", "op": "like", "val": "%" + this.get('title') + "%" }
+    ];
+
+    var search = Ember.$.ajax({
+      url: "http://localhost:5000/api/courseVersions",
+      data: { "q": JSON.stringify( { "filters": filters } ) },
+      dataType: "json",
+      contentType: "application/json"
+    });
+
+    search.success(function(data) { 
+      self.set('courseVersions', data.courseVersions);
+      alert(JSON.stringify(self.get('courseVersions')));
+    });
+
+  }.property('title'),
+
   actions: {
-    search: function() {
-      // TODO uhh
-      var queryObj = {
-        'filters': [
-          {'name': 'fullName', 'op': 'like', 'val': '%petyr%'}
-        ]
-      };
-      var queryString = '?q=' + JSON.stringify(queryObj);
-      var endpoint = '/api/instructors';
-      $.getJSON(endpoint + queryString, function(data) {
-        alert(JSON.stringify(data));
-      });
-    }
   }
+
 });
--- client/app/routes/search.js
+++ client/app/routes/search.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    title: {
+      refreshModel: false
+    }
+  },
   model: function() {
     return {
       instructors: this.store.find('instructor'),
-      // TODO Replace this part with the *actual* search resource..
       courseVersions: this.store.find('courseVersion')
     };
   }
