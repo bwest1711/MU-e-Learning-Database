01d72ad3e48f8faac3d333edf98ca99aab6f14bd
Brandon <bwest1711@gmail.com>
2015-10-23 09:57:43 -0400

    Moved images

--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -1,6 +1,4 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var Funnel   = require('broccoli-funnel');
-var Merge	 = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
@@ -22,19 +20,5 @@ module.exports = function(defaults) {
 
 	app.import('vendor/overrides/datatables.css');
 
-    var jqueryImages = new Funnel('.', {
- 		srcDir: '/bower_components/jquery-ui/themes/base/images/',
-		include: [ '*.png'],
-		destDir: '/assets/images'
-	});
-
-    var datatablesImages = new Funnel('.', {
- 		srcDir: '/vendor/Datatables/DataTables-1.10.9/images/',
-		include: [ '*.png'],
-		destDir: '/assets/images'
-	});
-
-    assets = new Merge([jqueryImages, datatablesImages]);
-
-	return app.toTree(assets);
+	return app.toTree();
 };
